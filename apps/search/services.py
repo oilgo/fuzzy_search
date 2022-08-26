4b57b3c2defad87b6dbc9b96945c9c57484009ab
oilgo <zinolya12@gmail.com>
2022-08-26 20:33:33 +0300

    new commit

--- apps/search/services.py
+++ apps/search/services.py
@@ -21,14 +21,20 @@ async def get_similar_values(value: SearchRequest):
     models = list(df.ModelName)
     length = len(models)
     if length == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"no data found in {value.file_name}")
+        detail = f"no data found in {value.file_name}"
+        error = ErrorRequest(404, detail, '127.0.0.1', os.getcwd() + '\\' + os.path.basename(__file__))
+        await error.write_log_errors()
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
     similarity_list = [0] * length
     for i in range(length):
         similarity_list[i] = round(similar(value.new_model, models[i]) * 100, value.round)
     output_df = pd.DataFrame(np.asarray(similarity_list), index=models, columns=['new_model'])
     output = output_df.loc[output_df['new_model'] >= value.percent].sort_values('new_model', ascending=False).head(value.limit)
     if output.empty:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="no data found for ypur request")
+        detail = "no data found for your request"
+        error = ErrorRequest(404, detail, '127.0.0.1', os.getcwd() + '\\' + os.path.basename(__file__))
+        await error.write_log_errors()    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
     return {
         'most_similar': output.to_dict()['new_model']
         }
